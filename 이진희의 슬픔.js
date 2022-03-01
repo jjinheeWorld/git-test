03c1457fa13f06fadc6837ceaef559e17e2f6936
jjinheeWorld <wlsgml450@naver.com>
2022-03-02 01:18:20 +0900

    눈물나는 뒹구루루 시

---
+++ 이진희의 슬픔.js
@@ -0,0 +1,24 @@
+console.log("세상 사람들은 알까? 내 슬픔을 ㅠㅠ");
+console.log("내 뒹구루루 잔디");
+console.log("10초 차이로 하루 못 만든거?");
+console.log("잔디에서 뒹굴다가 점프해야 해");
+console.log("뒹구루루루루루루루ㅜ루 ");
+console.log("심지어 급하게 올리다가 잘못 올렸어");
+
+console.log("뒹구루루루뒹구루루");
+console.log("삭제하려고 테스트 중인데 ");
+console.log("히스토리에 남겨져서 짜증나 ");
+console.log("그냥 못 본 척 넘어가볼까?");
+console.log("어차피 구멍난 잔디 만들거 알았으면");
+console.log("슬픈 여유로움을 즐기며 올렸을거야");
+
+console.log("얄리얄리 얄라셩 얄라리 얄라 아니야");
+console.log("나는");
+console.log("뒹구루루뒹구루루 뒹뒹구루루 야");
+
+console.log("하 구멍난 잔디는 이제 진정이 되었어.");
+console.log("나 좀 릴렉스 했다 이거야");
+
+console.log("근데 잘못 올린 파일은 어쩌징 ??????");
+
+console.log("뒹구루루뒹구구루루 ");
