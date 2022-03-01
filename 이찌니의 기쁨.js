f7d9c19eeeedf5d25323f19446beadfc89e20027
jjinheeWorld <wlsgml450@naver.com>
2022-03-02 02:26:30 +0900

    감쪽같이 삭제하는 방법

---
+++ 이찌니의 기쁨.js
@@ -0,0 +1,20 @@
+console.log("하이룽~");
+
+console.log("모야모야 히스토리에 흔적 감쪽같이 없애는 방법을 알아냈지모야");
+
+console.log("기분 좋아져서 나 오늘 연어 덮밥먹을거야 히히");
+
+console.log("룰루 랄라~");
+
+
+/*
+삭제할 commit 확인
+git log
+
+가장 마지막에 push한 commit 삭제
+git reset HEAD^
+
+commit을 지웠다는 것을 github 서버에 알려주어
+github 내에서도 해당 commit 삭제
+git push -f origin main
+*/
